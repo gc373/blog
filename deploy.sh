eb8612cc0c5d2b57a1e2a4188647f866d68749e9
gc373 <tosakan224@gmail.com>
2017-12-23 15:24:15 +0900

    update 2017/12/23 PM 03:24:15

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,18 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project. 
 hugo
+# Add changes to git.
+git add -A
 
+# Commit changes.
+msg="update `date +"%Y/%m/%d %p %I:%M:%S"`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+#Push source and build repos.
+git push origin master
 #↑ここを変更しましょう！！
 
 # Go To Public folder
