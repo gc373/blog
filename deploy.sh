17d59586be3522fcd8a14e0ec1545bc193b78998
gc373 <tosakan224@gmail.com>
2017-12-23 15:36:23 +0900

    update 2017/12/23 PM 03:36:23

--- deploy.sh
+++ deploy.sh
@@ -3,36 +3,39 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+function gitUpdate() {
+  # Add changes to git.
+  git add -A
+
+  # Commit changes.
+  msg="update `date +"%Y/%m/%d %p %I:%M:%S"`"
+  if [ $# -eq 1 ]
+    then msg="$1"
+  fi
+  git commit -m "$msg"
+
+  #Push source and build repos.
+  git push origin master
+}
+
+function gitRebuild(){
+  # Add changes to git.
+  git add -A
+
+  # Commit changes.
+  msg="rebuilding site `date +"%Y/%m/%d %p %I:%M:%S"`"
+  if [ $# -eq 1 ]
+    then msg="$1"
+  fi
+  git commit -m "$msg"
+
+  #Push source and build repos.
+  git push origin master
+}
+
 # Build the project. 
-hugo
-# Add changes to git.
-git add -A
-
-# Commit changes.
-msg="update `date +"%Y/%m/%d %p %I:%M:%S"`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-#Push source and build repos.
-git push origin master
-#↑ここを変更しましょう！！
-
+hugo && gitUpdate
 # Go To Public folder
-cd public
-# Add changes to git.
-git add -A
-
-# Commit changes.
-msg="rebuilding site `date +"%Y/%m/%d %p %I:%M:%S"`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-#Push source and build repos.
-git push origin master
-
+cd public && gitRebuild
 # Come Back
 cd ..
